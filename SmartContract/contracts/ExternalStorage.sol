679f702e394fcb0c4a2450761b04a99b53931d1f
Qianchen <yuqianchen93@gmail.com>
2017-11-14 13:19:40 +0100

    ExternalStorage is still a contract

--- SmartContract/contracts/ExternalStorage.sol
+++ SmartContract/contracts/ExternalStorage.sol
@@ -1,7 +1,58 @@
 pragma solidity ^0.4.8;
 
 contract ExternalStorage {
+
+  struct Portefeuille {
+    int     energyAccumul;
+    uint    balance;
+    uint    islandID;
+  }
+  
+  mapping(address=>Portefeuille) profil;
+  mapping(uint=>mapping(uint=>bool)) isConnected;
+
+  //event MeterLog(int energyDelta, uint changeAt);
+  // events should be placed in the main contract...
+
   function ExternalStorage() {
     // constructor
+  }
+
+  // Simple set and get functions
+
+  function setEnergy(address mAdr, int energyDelta) {
+    // require... assert the address is in the network...
+    profil[mAdr].energyAccumul += energyDelta;
+  }
+
+  function getEnergy(address mAdr) returns(int) {
+    return profil[mAdr].energyAccumul;
+  }
+
+  function setBalance(address mAdr, uint balanceDelta) {
+    // require... assert the address is in the network...
+    profil[mAdr].balance += balanceDelta;
+  }
+
+  function getEnergy(address mAdr) returns(uint) {
+    return profil[mAdr].balance;
+  }
+
+  function setIslandId(address mAdr, uint ilID) {
+    // require... assert the address is in the network...
+    profil[mAdr].islandID = ilID;
+  }
+
+  function getIslandId(address mAdr) returns(uint) {
+    return profil[mAdr].islandID;
+  }
+
+  function setConnection(uint _id1, uint _id2) {
+    //require(!getConnection(_id1, _id2));
+    isConnected[_id1][_id2] = true;
+  }
+
+  function getConnection(uint _id1, uint _id2) returns(bool) {
+    return isConnected[_id1][_id2];
   }
 }
--- SmartContract/contracts/Record.sol
+++ SmartContract/contracts/Record.sol
@@ -1,32 +1,70 @@
 pragma solidity ^0.4.8;
 
+import "./ExternalStorage.sol";
+
 contract Record {
-  address _id;
-  bytes   encryptedId;
+  /*address _id;
   int     power;
 
-  event RecordConsumption(bytes _id, uint timeStamp);
-  truevent RecordTransaction(bytes _from, bytes _to, uint timeStamp);
+  address generator;
+  address consumer;*/
+  address   adminAdr;
+  address   dataStorageAdr;
+  address[] meterAdrs;
 
-  modifier ownerOnly {
-    if (msg.sender == _id) {
+  event RecordConsumption(bytes _id, uint timeStamp);
+  event RecordTransaction(bytes _from, bytes _to, uint _measuredAt);
+
+  modifier producerOnly (int _energy){
+    if (_energy > 0) {
       _;
     } else {
       revert();
     }
   }
 
+  modifier participantOnly (address adr) {    // The address in in our network (in one of our energy islands)
+    if (assertMeterIn(adr)) {
+      _;
+    } else {
+      revert();
+    }
+  }
+  
   function Record() {
-    // constructor 
-    _id = msg.sender;
-    encryptedId = sha3(_id);
-    RecordConsumption(encryptedId, now);
+    // constructor
+    adminAdr = msg.sender;
+    dataStorageAdr = new ExternalStorage();
     
   }
 
-  function setPower(uint _p) ownerOnly {
-    power = _p;
+  function addMeter(address mAdr, uint ilID) {
+    require(!assertMeterIn(mAdr));
+    meterAdrs.push(mAdr);
+    profil[mAdr] = Portefeuille(0,0,ilID);
+
+    // For now we do not delete the smart meter or disenable it..... (it mighted be a dead smart meter placed in the network)
   }
+
+
+  function energyTransactRecord(address _to, uint _p) participantOnly(_to) participantOnly(msg.sender) returns (bool) {
+    //require(assertMeterIn(msg.sender));
+    address requester = msg.sender;
+    ExternalStorage(dataStorageAdr).getConnection()
+    
+
+  }
+
+  // Assert information
+  function assertMeterIn(address adr) returns(bool) {
+    for (var i = 0; i < meterAdrs.length; i++) {
+      if (meterAdrs[i] == adr) {
+        return true;
+      }
+    }
+    return false;
+  }
+
 
   // TestOnly
   function getPower() {
