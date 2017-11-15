f12b3b4be8de295002bdefc35ca1cecf484ec127
ThulasiKrishna <thulasikrishna.rr@gmail.com>
2017-11-15 09:07:06 +0100

    Add files via upload

---
+++ HTML/pages/jane_device_1.php
@@ -0,0 +1,38 @@
+<!-- Content Table -->
+<div class="card mb-3">
+  <div class="card-header">
+    <i class="fa fa-table"></i>
+    Jane's devices
+  </div>
+  <div class="card-body">
+    <div class="table-responsive">
+      <table class="table table-bordered" width="100%" id="janeDev1DataTable" name="janeDev1DataTable" cellspacing="0">
+        <thead>
+          <tr>
+            <th>Timestamp</th>
+            <th>TX-Hash</th>
+            <th>Price</th>
+            <th>Energy (kWh)</th>
+            <th>Partner device</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>2017-08-25T08:00:00+00:00</td>
+            <td><a href="https://rinkeby.etherscan.io/tx/0xe23fe415b269e4f5fbf5cb4e75e772db01d55c579cb23a82a0b336702e0782cd">0xe23fe415b269e4f5fbf5cb4e75e772db01d55c579cb23a82a0b336702e0782cd</a></td>
+            <td>0.004</td>
+            <td>0.1</td>
+			<td>Main Socket</td>
+          </tr>
+            <tr>
+            <td>2017-08-25T06:15:00+00:00</td>
+            <td><a href="https://rinkeby.etherscan.io/tx/0x56fa60af2ed2379d4b27b9421d513b87d372d1a5e9dfbfabbac43ecf45231a1a">0x56fa60af2ed2379d4b27b9421d513b87d372d1a5e9dfbfabbac43ecf45231a1a</a></td>
+            <td>0.005</td>
+            <td>0.4</td>
+			<td>Battery storage</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
